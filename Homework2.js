1d9b5511075e290adebe2735fc7bfebf0b27d54a
Arthur Bazoyan <arthur_bazoyan@edu.aua.am>
2017-09-09 17:46:00 +0400

    First Commit

---
+++ Homework2.js
@@ -0,0 +1,10 @@
+console.log("This is a test text");
+alert ("YO!");
+const num1 = 20;
+const num2 = -16;
+if ( (num1 + num2) > 3)
+{
+console.log ( "Yeah 4 > 3" );
+}
+console.log ( num1 * num2 );
+console.log ( num2 / num1 );
